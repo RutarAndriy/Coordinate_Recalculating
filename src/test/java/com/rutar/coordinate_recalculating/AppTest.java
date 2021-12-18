ee548df6e42b06e5fc0ceb4c78b3a32059cce22b
RutarAndriy <RutarAndriy@gmail.com>
2021-12-18 14:56:58 +0200

    Generated project structure

---
+++ src/test/java/com/rutar/coordinate_recalculating/AppTest.java
@@ -0,0 +1,19 @@
+package com.rutar.coordinate_recalculating;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+// ............................................................................
+
+public class AppTest {
+
+///////////////////////////////////////////////////////////////////////////////
+
+@Test
+public void should_Answer_With_True()
+    { assertTrue(true); }
+
+///////////////////////////////////////////////////////////////////////////////
+
+}
